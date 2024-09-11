86694c86c69b451dabbe5cb40b8951204f5dcdb3
Pop2323 <omar4500455@gmail.com>
2024-09-11 10:58:32 +0300

    Update-4

--- js/script.js
+++ js/script.js
@@ -7,3 +7,18 @@ document.addEventListener('DOMContentLoaded', () => {
         navMenu.classList.toggle('active');
     });
 });
+
+// This Button Is Used To Scroll To Top
+const Top = document.getElementById('Top');
+//On Clicking, The Function Will Run 
+Top.addEventListener("click", function() {
+//After Clicking On The Button, The Page Will Be In The Top = (0) 
+	window.scrollTo({
+		top: 0,
+		left: 0,
+//It Keeps The Scroll Smooth 
+		behavior: "smooth" 
+	});
+});
+//Add Event (scroll)
+document.addEventListener("scroll", Active_Class);
